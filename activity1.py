c9545dc7392c0eb417516564c0fe23d66e05578c
mustafa276 <mmustafamajeed1@gmail.com>
2024-08-21 12:19:02 +0500

    operators

---
+++ activity1.py
@@ -0,0 +1,10 @@
+tree1 = 56
+tree2 = 78
+tree3 = 35
+tree4 = 21
+tree5 = 99
+print("tree1 = 56, \ntree2 = 78, \ntree3 = 35, \ntree4 = 21, \ntree5 = 99")
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the numbers is", sum)
+average = sum/5
+print("the average of all numbers is", average)
